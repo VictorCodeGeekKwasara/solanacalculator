be56927dc4e73ac64b52dad09bfc5203dfadaf5d
professor_cypher <professorcodegeek@DESKTOP-87DPNVK.localdomain>
2022-08-31 11:13:16 +0200

    simple calculator anchor

---
+++ programs/calculator/src/lib.rs
@@ -0,0 +1,71 @@
+use anchor_lang::prelude::*;
+
+declare_id!("JDeZu9xoJEn8FhgTwbKkvNx2VihmT45eZ9AgyiafzpDc");
+
+#[program]
+pub mod calculator {
+    use super::*;
+
+    pub fn initialize(ctx: Context<SetupCalculator>) -> Result<()> {
+        ctx.accounts.calculator.value = 0 ;        
+        Ok(())
+    }
+    pub fn sum(ctx:Context<Evaluate>, unit: i64) -> Result<()> {
+        ctx.accounts.calculator.add(unit) ;        
+        Ok(())
+    }
+    pub fn subtract(ctx:Context<Evaluate>, unit: i64) -> Result<()> {
+        ctx.accounts.calculator.remove(unit);        
+        Ok(())
+    }
+    pub fn multiply(ctx:Context<Evaluate>, unit: i64) -> Result<()> {
+        ctx.accounts.calculator.multiply(unit);        
+        Ok(())
+    }
+    pub fn divide(ctx:Context<Evaluate>, unit: i64) -> Result<()> {
+        ctx.accounts.calculator.divide_abs(unit);        
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct SetupCalculator<'info> {
+    #[account( init,payer=user, space = 27)]
+   pub calculator : Account<'info,Calculator>,
+    #[account(mut)]
+   pub user: Signer<'info>,
+   pub system_program: Program<'info,System> 
+}
+
+#[derive(Accounts)]
+pub struct Evaluate<'info> {
+    #[account(mut)]
+    pub calculator : Account<'info,Calculator>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct Calculator {
+    value: i64
+}
+
+impl Calculator {
+
+    pub fn add (&mut self, unit: i64) {
+        self.value = self.value + unit ;
+    }
+
+    pub fn remove (&mut self, unit: i64) {
+        self.value = self.value - unit ;
+    }
+    pub fn multiply (&mut self, unit: i64) {
+        self.value = self.value * unit ;
+    }
+
+    pub fn divide_abs (&mut self, unit: i64) {
+        self.value = self.value / unit ;
+    }
+
+
+
+}
---
+++ tests/calculator.ts
@@ -0,0 +1,91 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { expect } from "chai";
+import { Calculator } from "../target/types/calculator";
+
+describe("calculator", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Calculator as Program<Calculator>;
+  const calculatorKeypair = anchor.web3.Keypair.generate();
+  const user =(program.provider as anchor.AnchorProvider).wallet;
+
+  it("Is initialized!", async () => {
+    // Add your test here.   
+
+   const tx = await program.methods.initialize().accounts({
+      calculator: calculatorKeypair.publicKey,
+      user: user.publicKey
+    }).signers([calculatorKeypair])
+    .rpc();
+    console.log("To infinity and beyond!! " + tx)
+
+    let calculatorState = await program.account.calculator.fetch(
+			calculatorKeypair.publicKey
+		);
+      console.log('The value is', calculatorState.value.toString()); 
+  });
+  it("can add values", async ()=>{
+
+    await program.methods
+			.sum(new anchor.BN(4))
+			.accounts({
+				calculator: calculatorKeypair.publicKey,
+			})
+			.rpc();
+
+    let calculatorState = await program.account.calculator.fetch(
+      calculatorKeypair.publicKey
+    );
+    console.log('The value is', calculatorState.value.toString());
+
+  })
+  it("can remove values", async ()=>{
+
+    await program.methods
+			.subtract(new anchor.BN(8))
+			.accounts({
+				calculator: calculatorKeypair.publicKey,
+			})
+			.rpc();
+
+    let calculatorState = await program.account.calculator.fetch(
+      calculatorKeypair.publicKey
+    );
+    console.log('The value is', calculatorState.value.toString());
+
+  })
+  it("can multiply values", async ()=>{
+
+    await program.methods
+			.multiply(new anchor.BN(-6))
+			.accounts({
+				calculator: calculatorKeypair.publicKey,
+			})
+			.rpc();
+
+    let calculatorState = await program.account.calculator.fetch(
+      calculatorKeypair.publicKey
+    );
+    console.log('The value is', calculatorState.value.toString());
+
+  })
+  it("can divide values absolutely", async ()=>{
+
+    await program.methods
+			.divide(new anchor.BN(5))
+			.accounts({
+				calculator: calculatorKeypair.publicKey,
+			})
+			.rpc();
+
+    let calculatorState = await program.account.calculator.fetch(
+      calculatorKeypair.publicKey
+    );
+    console.log('The value is', calculatorState.value.toString());
+
+  })
+
+    
+});
